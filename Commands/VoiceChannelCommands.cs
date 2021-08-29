d629dc416e5bfc3cb4735ef19f5bd8c74d2fc103
Mattias Mikkola <mattias.mikkola@shoppa.com>
2021-08-29 21:08:03 +0200

    Getting logging and voice connect going

---
+++ Commands/VoiceChannelCommands.cs
@@ -0,0 +1,38 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace YAMBot.Commands
+{    
+    class VoiceChannelCommands : BaseCommandModule
+    {
+        [Command("join")]
+        public async Task JoinCommand(CommandContext commandContext, DiscordChannel channel = null)
+        {
+            channel ??= commandContext.Member.VoiceState?.Channel;
+            var voiceNextConnection = await channel.ConnectAsync();
+            voiceNextConnection.VoiceReceived += VoiceNextConnection_VoiceReceived;
+            voiceNextConnection.UserSpeaking += VoiceNextConnection_UserSpeaking;
+        }
+
+        private async Task VoiceNextConnection_UserSpeaking(VoiceNextConnection sender, DSharpPlus.EventArgs.UserSpeakingEventArgs e)
+        {
+            Log.Debug($"UserSpeaking\tSSR: {e.SSRC}\tUser: {e.User.Id}");
+        }
+
+        private async Task VoiceNextConnection_VoiceReceived(VoiceNextConnection sender, DSharpPlus.VoiceNext.EventArgs.VoiceReceiveEventArgs e)
+        {
+            Log.Debug($"VoiceReceived\tSSR: {e.SSRC}\tDur: {e.AudioDuration}ms");
+        }
+
+        [Command("leave")]
+        public async Task LeaveCommand(CommandContext commandContext)
+        {
+            VoiceNextConnection connection = commandContext.Client.GetVoiceNext().GetConnection(commandContext.Guild);
+            connection.Disconnect();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using DSharpPlus;
+using DSharpPlus.VoiceNext;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using YAMBot.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace YAMBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()                
+                .CreateLogger();            
+
+            ILoggerFactory loggerFactory = new LoggerFactory().AddSerilog();
+
+            if (!configuration.GetSection("yambottoken").Exists())
+            {
+                return;
+            }
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            { 
+                Token = configuration.GetSection("yambottoken").Value,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged,
+                LoggerFactory = loggerFactory
+            });
+
+            CommandsNextExtension commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            });
+
+            commands.RegisterCommands<VoiceChannelCommands>();
+
+            discord.UseVoiceNext(new VoiceNextConfiguration()
+            {
+                EnableIncoming = true,               
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
